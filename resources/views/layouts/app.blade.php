c56b830126e8a39d0635fce4cb66b1033f044613
Saut Josua Romartua <questlee88@gmail.com>
2026-06-19 15:18:23 +0700

    fix: struktur penuh vercel json dengan env railway

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Web Profile')</title>
+    
     <link rel="stylesheet" href="{{ asset('bootstrap-5.3.8-dist/css/bootstrap.min.css') }}">
+    
     <style>
         :root {
             --navy: #081222;
@@ -65,7 +67,7 @@
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a href="{{ route('home') }}" class="nav-link">Home</a>
-                    </li>
+                    </li >
                     <li class="nav-item">
                         <a href="{{ route('projects') }}" class="nav-link">Projects</a>
                     </li>
